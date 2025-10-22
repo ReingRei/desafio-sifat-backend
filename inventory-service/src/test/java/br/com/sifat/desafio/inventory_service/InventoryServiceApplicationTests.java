cea36620d4cfb3c144e6296d4782c879734936c0
Reinaldo Zenelato <reinaldofilho76@gmail.com>
2025-10-22 12:51:28 -0300

    feat: Setup initial project structure

---
+++ inventory-service/src/test/java/br/com/sifat/desafio/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sifat.desafio.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
